8522691d249b58ff034a84f21546440b9d51ead5
christophe hubeaux <ch.hubeaux@gmail.com>
2024-03-19 17:56:42 +0100

    add form game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,25 @@
 class GamesController < ApplicationController
+    def new
+        @game = Game.new
+    end
+
+    def create
+        @game = Game.new(game_params)
+        if @game.save
+          redirect_to dashboard_game_path(@game), notice: 'La partie a été créée avec succès.'
+        else
+          render :new
+        end
+    end
+    
+    def dashboard
+        @game = Game.find(params[:id])
+    end
+
+    private
+    
+    def game_params
+        params.require(:game).permit(:name)
+    end
+
 end
