61691c0e464f6f1fa6292e0b4b2173043f9dc1fa
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-21 17:25:55 +0100

    login issue ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @user_game = UsersGame.find_by(user_id: current_user)
-    @game = @user_game.game
+    @user_game = UsersGame.find_by(user: current_user)
+    @game = @user_game.game unless @user_game.nil?
   end
 end
