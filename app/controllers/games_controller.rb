0e790d3ea9fc8bc9a4ba0d359acb1b27d21b8cde
christophe hubeaux <ch.hubeaux@gmail.com>
2024-03-22 22:51:36 +0100

    add code, validation for game & hover on validate btn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,7 +9,7 @@ class GamesController < ApplicationController
     if @game.save
       redirect_to dashboard_game_path(@game), notice: 'La partie a été créée avec succès.'
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -99,6 +99,6 @@ class GamesController < ApplicationController
   # end
 
   def game_params
-    params.require(:game).permit(:name)
+    params.require(:game).permit(:name, :code)
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,8 @@ class Game < ApplicationRecord
   has_many :users_games
   has_many :pronostics
   has_many :users, through: :users_games
+
+  validates :name, presence: true
+  validates :code, presence: true, length: { minimum: 5, too_short: "Minimum 5 caractères" }
+
 end
