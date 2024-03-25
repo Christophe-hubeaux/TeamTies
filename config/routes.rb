36e3cf64ae20e4b66fae502f507c5378b0017eb0
Sylvain Bohrer <a3e7d6@gmail.com>
2024-03-25 18:15:34 +0100

    Add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+
+  devise_for :users, controllers: { registrations: "users/registrations" }
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 
   resources :games, only: %i[index new create] do # /games GET user-games index, /games/new GET games new, /game/new POST games create
     resources :teams, only: [:index]
@@ -21,7 +19,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users_games, only: [:new, :create]
+  resources :users_games, only: %i[new create]
 
   namespace :organisateur do
     resources :games, only: [:edit] # /organisateur/games/edit
