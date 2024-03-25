2b95459dba8980bbffa10f33284ce56d63d1ab4d
Christophe-hubeaux <154839320+Christophe-hubeaux@users.noreply.github.com>
2024-03-25 09:30:24 +0100

    Update game.rb

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,4 @@ class Game < ApplicationRecord
   has_many :users_games
   has_many :pronostics
   has_many :users, through: :users_games
-
-  validates :name, presence: true
-  validates :code, presence: true, length: { minimum: 5, too_short: "Minimum 5 caractères" }
-
 end
