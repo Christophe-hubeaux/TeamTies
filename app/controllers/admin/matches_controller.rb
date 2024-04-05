a0f65187f95a525448650c498697f130e836d4a7
christophe hubeaux <ch.hubeaux@gmail.com>
2024-04-05 17:02:01 +0200

    ajout admin pour encoder les scores

---
+++ app/controllers/admin/matches_controller.rb
@@ -0,0 +1,21 @@
+class Admin::MatchesController < ApplicationController
+
+    def index
+        @matches = Match.all
+    end
+    
+    def update
+        @match = Match.find(params[:id])
+        if @match.update(match_params)
+          redirect_to :admin_matches, notice: 'Match mis à jour avec succès.'
+        else
+          render :edit
+        end
+    end
+
+    private
+
+    def match_params
+        params.require(:match).permit(:home_team_result, :away_team_result)
+    end
+end
