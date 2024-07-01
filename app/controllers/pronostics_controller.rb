e67c5cc8ac1e466760bf5af7497299614486b713
christophe hubeaux <ch.hubeaux@gmail.com>
2024-07-01 11:26:00 +0200

    more security

--- app/controllers/pronostics_controller.rb
+++ app/controllers/pronostics_controller.rb
@@ -2,28 +2,33 @@ class PronosticsController < ApplicationController
     def create
         @game = Game.find(params[:game_id])
         @match = Match.find(params[:match_id])
-        @date = @match.date.strftime("%d/%m")
-        @prono = Pronostic.find_or_initialize_by(user: current_user, match: @match, game: @game)
-        @prono.assign_attributes(prono_params)
+        if ((@match.date - Time.now) >= 130*60)
 
-        # @prono = Pronostic.new(prono_params)
-        # @prono.user = current_user
-        # @prono.match = Match.find(params[:match_id])
-        # @date = @prono.match.date.strftime("%d/%m")
-        @prono.game = @game
-        if @prono.save
-            # redirect_to game_matches_path(@game, anchor: @date)
-        # elsif @prono.save
-        else
-            render "matches/index", status: :unprocessable_entity
+            @date = @match.date.strftime("%d/%m")
+            @prono = Pronostic.find_or_initialize_by(user: current_user, match: @match, game: @game)
+            @prono.assign_attributes(prono_params)
+
+            # @prono = Pronostic.new(prono_params)
+            # @prono.user = current_user
+            # @prono.match = Match.find(params[:match_id])
+            # @date = @prono.match.date.strftime("%d/%m")
+            @prono.game = @game
+            if @prono.save
+                # redirect_to game_matches_path(@game, anchor: @date)
+            # elsif @prono.save
+            else
+                render "matches/index", status: :unprocessable_entity
+            end
         end
     end
 
     def update
         @game = Game.find(params[:game_id])
         @match = Match.find(params[:match_id])
-        @prono = Pronostic.where(match_id: @match, game_id: @game, user_id: current_user)
-        @prono.update(prono_params)
+        if ((@match.date - Time.now) >= 130*60)
+            @prono = Pronostic.where(match_id: @match, game_id: @game, user_id: current_user)
+            @prono.update(prono_params)
+        end
     end
 
     private
